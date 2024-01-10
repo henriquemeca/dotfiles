f7b185b79874debf90c74eae3f82dfd658c1708c
Henrique Brito Silva <henriquebritos@hotmail.com>
2024-01-10 13:57:07 -0300

    chore: upgrading symbolic links script

--- setup_symbolic_links.sh
+++ setup_symbolic_links.sh
@@ -2,24 +2,30 @@
 
 ### Add symlinks to all "./config" subfolders
 for dir in $(find "config" -maxdepth 1 -mindepth 1 -type d); do
+    source_dir="$(pwd)/$dir" 
     target_dir="$HOME/.$dir"
-   if [[ -h $target_dir ]]; then
-       echo "Symbolic link for $dir already exists in $target_dir" 
-   else
-       echo "Creating symbolic link for $target_dir folder"
-       ln -s "$(pwd)/$dir" $target_dir
-       echo "Link created"
-   fi
+   
+    if [[ -h $target_dir ]]; then
+        echo "Symbolic link already exists for $source_dir in $target_dir" 
+    else
+        echo "Creating symbolic link from $source_dir to $target_dir"
+        ln -s $source_dir $target_dir
+        echo "Link created"
+    fi
 done
 
 ### Add packer.nvim symbolic link 
 nvim_start_path="local/share/nvim/site/pack/packer/start"
 nvim_packer_path="$nvim_start_path/packer.nvim"
 
+source_dir="$(pwd)/$nvim_packer_path"
+target_dir="$HOME/.$nvim_packer_path"
+
 mkdir -p "$HOME/.$nvim_start_path"
-if [[ -h "$HOME/.$nvim_packer_path" ]]; then
-    ln -s "$(pwd)/.$nvim_packer_path" "$HOME/.$nvim_packer_path"
-    echo "packer.nvim symbolic link created"
+if [[ -h $target_dir ]]; then
+    echo "Symbolic link already exists for $source_dir in $target_dir" 
 else
-    echo "Symbolic link for packer.nvim already exists in $HOME/.$nvim_packer_path" 
+    echo "Creating link from $source_dir to $target_dir"
+    ln -s $source_dir $target_dir
+    echo "Link created"
 fi
